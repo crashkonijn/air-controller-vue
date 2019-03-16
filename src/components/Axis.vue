6f913e255d6803d065c29e609aebf80841de3729
Peter Klooster <develop@pklooster.nl>
2019-03-16 16:26:53 +0100

    Improved Axis state handling

--- src/components/Axis.vue
+++ src/components/Axis.vue
@@ -1,5 +1,9 @@
 <template xmlns:v-hammer="http://www.w3.org/1999/xhtml">
-    <button :class="classes" v-hammer:press="down" v-hammer:pressup="up">
+    <button :class="classes"
+            v-hammer:press="press"
+            v-hammer:pressup="pressUp"
+            v-hammer:pan.end="panEnd"
+    >
         <slot>{{ id || name }}: {{ mode }}</slot>
     </button>
 </template>
@@ -26,9 +30,21 @@
                 }
             }
         },
+        data:() => ({
+            isPressed: false
+        }),
         computed: {
             classes() {
-                return [this.baseClasses, this.anchorClasses].join(' ');
+                return [this.baseClasses, this.pressedClasses, this.anchorClasses].join(' ');
+            },
+            pressedClasses() {
+                if (!this.isPressed) {
+                    return '';
+                }
+
+                let component = this.kebab_case(this.$options.name);
+
+                return component + '--pressed';
             },
             vectors() {
                 return {
@@ -48,10 +64,21 @@
             }
         },
         methods: {
+            press() {
+                this.down();
+            },
+            pressUp() {
+                this.up();
+            },
+            panEnd() {
+                this.up();
+            },
             down() {
-                if (this.getPremium()) {
+                if (this.getPremium() || this.isPressed) {
                     return;
                 }
+
+                this.isPressed = true;
 
                 this.$store.commit('addAxis', {
                     id: this._uid,
@@ -64,10 +91,14 @@
                     return;
                 }
 
-                this.$store.commit('removeAxis', {
-                    id: this._uid,
-                    key: this.name
-                });
+                setTimeout(() => {
+                    this.isPressed = false;
+
+                    this.$store.commit('removeAxis', {
+                        id: this._uid,
+                        key: this.name
+                    });
+                }, 10);
             },
         }
     }
