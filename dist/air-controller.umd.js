40be3e1062734ea9f2179607452a3168568b37ee
Peter Klooster <develop@pklooster.nl>
2019-03-17 16:25:50 +0100

    Updated build

--- dist/air-controller.umd.js
+++ dist/air-controller.umd.js
@@ -25034,7 +25034,9 @@ external_commonjs_vue_commonjs2_vue_root_Vue_default.a.use(vuex_esm);
           });
         }
       },
-      message: function message(target, data) {},
+      message: function message(target, data) {
+        console.log(data);
+      },
       onDeviceMotion: null
     },
     deviceId: -1,
@@ -25974,6 +25976,113 @@ var Gyroscope_component = normalizeComponent(
 )
 
 /* harmony default export */ var Gyroscope = (Gyroscope_component.exports);
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"d1b3d832-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Dragstick.vue?vue&type=template&id=04eed410&xmlns%3Av-hammer=http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml&
+var Dragstickvue_type_template_id_04eed410_xmlns_3Av_hammer_http_3A_2F_2Fwww_w3_org_2F1999_2Fxhtml_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{directives:[{name:"hammer",rawName:"v-hammer:pan.all",value:(_vm.pan),expression:"pan",arg:"pan",modifiers:{"all":true}},{name:"hammer",rawName:"v-hammer:panstart",value:(_vm.panStart),expression:"panStart",arg:"panstart"},{name:"hammer",rawName:"v-hammer:panend",value:(_vm.panEnd),expression:"panEnd",arg:"panend"}],class:_vm.classes},[_c('svg',{staticStyle:{"width":"100%","height":"100%"}},[_c('circle',{attrs:{"cx":_vm.joystickPosition.x,"cy":_vm.joystickPosition.y,"r":"40","stroke":"black","fill":"#4286f4","opacity":"0.4"}}),_c('circle',{attrs:{"cx":_vm.dragPosition.x,"cy":_vm.dragPosition.y,"r":"40","stroke":"black","fill":"#4286f4","opacity":"0.4"}})])])}
+var Dragstickvue_type_template_id_04eed410_xmlns_3Av_hammer_http_3A_2F_2Fwww_w3_org_2F1999_2Fxhtml_staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/Dragstick.vue?vue&type=template&id=04eed410&xmlns%3Av-hammer=http%3A%2F%2Fwww.w3.org%2F1999%2Fxhtml&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Dragstick.vue?vue&type=script&lang=js&
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+/* harmony default export */ var Dragstickvue_type_script_lang_js_ = ({
+  name: 'AirJoystick',
+  mixins: [base, anchor_anchor, premium_premium],
+  mounted: function mounted() {
+    this.$store.commit('setChannelSetting', {
+      name: this.name,
+      channel: 'continues'
+    });
+  },
+  data: function data() {
+    return {
+      dragPosition: {
+        x: 0,
+        y: 0
+      },
+      joystickPosition: {
+        x: 0,
+        y: 0
+      },
+      visualPosition: {
+        x: 0,
+        y: 0
+      },
+      dir: null
+    };
+  },
+  computed: {
+    classes: function classes() {
+      return [this.id, this.baseClasses, this.anchorClasses].join(' ');
+    }
+  },
+  methods: {
+    panStart: function panStart(e) {
+      this.dragPosition = this.getVictor(e);
+      this.joystickPosition = this.getVictor(e);
+    },
+    panEnd: function panEnd() {
+      this.$store.commit('removeAxis', {
+        id: this._uid,
+        key: this.name
+      });
+    },
+    pan: function pan(e) {
+      this.dragPosition = this.getVictor(e);
+      this.updateJoystickPosition();
+      this.dir = this.dragPosition.clone().subtract(this.joystickPosition).normalize();
+      this.$store.commit('addAxis', {
+        id: this._uid,
+        value: this.dir,
+        key: this.name
+      });
+    },
+    getVictor: function getVictor(e) {
+      return new victor_default.a(e.center.x, e.center.y);
+    },
+    updateJoystickPosition: function updateJoystickPosition() {
+      if (this.dragPosition.distance(this.joystickPosition) > 50) {
+        var dir = this.dragPosition.clone().subtract(this.joystickPosition);
+        this.joystickPosition = this.dragPosition.clone().subtract(dir.normalize().multiply(new victor_default.a(50, 50)));
+      }
+    }
+  }
+});
+// CONCATENATED MODULE: ./src/components/Dragstick.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_Dragstickvue_type_script_lang_js_ = (Dragstickvue_type_script_lang_js_); 
+// CONCATENATED MODULE: ./src/components/Dragstick.vue
+
+
+
+
+
+/* normalize component */
+
+var Dragstick_component = normalizeComponent(
+  components_Dragstickvue_type_script_lang_js_,
+  Dragstickvue_type_template_id_04eed410_xmlns_3Av_hammer_http_3A_2F_2Fwww_w3_org_2F1999_2Fxhtml_render,
+  Dragstickvue_type_template_id_04eed410_xmlns_3Av_hammer_http_3A_2F_2Fwww_w3_org_2F1999_2Fxhtml_staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var Dragstick = (Dragstick_component.exports);
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"d1b3d832-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Joystick.vue?vue&type=template&id=3a3cccb2&
 var Joystickvue_type_template_id_3a3cccb2_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{ref:"joystick",class:_vm.classes},[_c('div',{staticClass:"children"},[_vm._t("default")],2)])}
 var Joystickvue_type_template_id_3a3cccb2_staticRenderFns = []
@@ -26430,12 +26539,14 @@ var Data_component = normalizeComponent(
 
 
 
+
 /* harmony default export */ var components = ({
   install: function install(Vue) {
     Vue.component('air-axis', Axis);
     Vue.component('air-button', Button);
     Vue.component('air-controller', Controller);
     Vue.component('air-gyroscope', Gyroscope);
+    Vue.component('air-dragstick', Dragstick);
     Vue.component('air-joystick', Joystick);
     Vue.component('air-page', Page);
     Vue.component('air-pan', Pan);
